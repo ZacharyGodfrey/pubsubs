34f4d9d966dc5c3aa5391b4f811827675d895778
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2024-10-30 07:18:38 -0700

    WIP

--- src/build.js
+++ src/build.js
@@ -3,19 +3,15 @@ import Mustache from 'mustache';
 import { emptyFolder, copy, readFile, writeFile } from '../lib/file.js';
 
 emptyFolder('dist');
+copy('src/assets/favicon.png', 'dist/favicon.png');
 copy('data.json', 'dist/data.json');
 
 const template = readFile('src/assets/page.html');
-
 const data = JSON.parse(readFile('data.json'));
+const style = readFile('src/assets/style.css');
 
 data.price = data.priceLine.match(/\$\d+\.*\d*/)[0];
 
-const partials = {
-  favicon: readFile('src/assets/favicon.png', 'base64'),
-  style: readFile('src/assets/style.css'),
-};
-
-const output = Mustache.render(template, data, partials);
+const output = Mustache.render(template, data, { style });
 
 writeFile('dist/index.html', output);
